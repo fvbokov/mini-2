9b9d9e37b2e0ed23d141397bc20fd78f493a69f3
Fedor <58223445+Marginalisimus@users.noreply.github.com>
2020-11-04 21:01:19 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3 {
+	public partial class Form1 : Form {
+
+		bool draw, drag;
+		int x, y, shiftX, shiftY;
+		static int radius;
+		static Color color;
+		string which;
+
+		Node figure;
+
+		public Form1() {
+			InitializeComponent();
+			draw = false;
+			figure = new CircleNode();
+			which = "circle";
+			drag = false;
+			shiftX = 0;
+			shiftY = 0;
+		}
+
+		static Form1() {
+			radius = 50;
+			color = Color.Green;
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e) {
+			Graphics g = e.Graphics;
+			//g.Clear(Color.Beige);
+			if (draw) {
+				figure.DrawFigure(e.Graphics);
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e) {
+			figure.radius = radius;
+			Invalidate();
+		}
+
+		private void квадратToolStripMenuItem_Click(object sender, EventArgs e) {
+			which = "square";
+			x = figure.X;
+			y = figure.Y;
+			figure = new SquareNode(x, y);
+			Invalidate();
+		}
+
+		private void кругToolStripMenuItem_Click(object sender, EventArgs e) {
+			which = "circle";
+			x = figure.X;
+			y = figure.Y;
+			figure = new CircleNode(x, y);
+			Invalidate();
+		}
+
+		private void треугольникToolStripMenuItem_Click(object sender, EventArgs e) {
+			which = "tri";
+			x = figure.X;
+			y = figure.Y;
+			figure = new TriangleNode(x, y);
+			Invalidate();
+		}
+
+		private void Form1_MouseUp(object sender, MouseEventArgs e) {
+			if (drag) {
+				drag = false;
+				Invalidate();
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e) {
+			if (drag) {
+				figure.X = e.X - shiftX;
+				figure.Y = e.Y - shiftY;
+				Invalidate();
+			}
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e) {
+			if (figure.Contains(e.X, e.Y) && draw) {
+				if (MouseButtons.Right == e.Button) {
+					draw = false;
+					drag = false;
+				}
+
+				if (MouseButtons.Left == e.Button) {
+					shiftX = e.X - figure.X;
+					shiftY = e.Y - figure.Y;
+
+					drag = true;
+				}
+			}
+
+			else {
+				if (MouseButtons.Left == e.Button) {
+					if (which == "circle") figure = new CircleNode(e.X, e.Y);
+					if (which == "square") figure = new SquareNode(e.X, e.Y);
+					if (which == "tri") figure = new TriangleNode(e.X, e.Y);
+					figure.X = e.X;
+					figure.Y = e.Y;
+					draw = true;
+				}
+			}
+			Invalidate();
+		}
+	}
+}
---
+++ Node.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3 {
+
+    abstract class Node {
+        protected static int size;
+        protected int posX;
+        protected int posY;
+
+        public int X {
+            get {
+                return posX;
+            }
+            set {
+                posX = value;
+            }
+        }
+        public int Y {
+            get {
+                return posY;
+            }
+            set {
+                posY = value;
+            }
+        }
+        public int radius {
+            get {
+                return radius;
+            }
+            set {
+                size = value;
+            }
+        }
+
+        abstract public void DrawFigure(Graphics g);
+        abstract public bool Contains(int Mx, int My);
+        public Node(int x = 0, int y = 0) {
+            posX = x;
+            posY = y;
+        }
+    }
+
+    class TriangleNode : Node {
+        override public void DrawFigure(Graphics g) {
+            Point top = new Point(posX, posY - size / 2);
+            Point left = new Point(posX - size / 2, posY + size);
+            Point right = new Point(posX + size / 2, posY + size);
+
+            Point[] trianglePoints = { top, left, right };
+
+            g.FillPolygon(new SolidBrush(Color.Green), trianglePoints);
+        }
+
+        public bool IsInPolygon(Point[] poly, Point p) {//https://stackoverflow.com/questions/4243042/c-sharp-point-in-polygon
+            Point p1, p2;
+            bool inside = false;
+
+            if (poly.Length < 3) {
+                return inside;
+            }
+
+            Point oldPoint = new Point(poly[poly.Length - 1].X, poly[poly.Length - 1].Y);
+
+            for (int i = 0; i < poly.Length; i++) {
+                Point newPoint = new Point(poly[i].X, poly[i].Y);
+
+                if (newPoint.X > oldPoint.X) {
+                    p1 = oldPoint;
+                    p2 = newPoint;
+                }
+                else {
+                    p1 = newPoint;
+                    p2 = oldPoint;
+                }
+
+                if ((newPoint.X < p.X) == (p.X <= oldPoint.X) && (p.Y - (long)p1.Y) * (p2.X - p1.X) < (p2.Y - (long)p1.Y) * (p.X - p1.X)) {
+                    inside = !inside;
+                }
+
+                oldPoint = newPoint;
+            }
+
+            return inside;
+        }
+
+        public override bool Contains(int Mx, int My) {
+            Point top = new Point(posX, posY - size / 2);
+            Point left = new Point(posX - size / 2, posY + size);
+            Point right = new Point(posX + size / 2, posY + size);
+
+            Point[] trianglePoints = { top, left, right,  };
+            if (IsInPolygon(trianglePoints, new Point(Mx, My))) return true;
+            else return false;
+        }
+
+        public TriangleNode(int x = 0, int y = 0) : base(x, y) { }
+    }
+
+    class CircleNode : Node {
+        override public void DrawFigure(Graphics g) {
+            g.FillEllipse(new SolidBrush(Color.Green), new Rectangle(posX - size / 2, posY - size / 2, size, size));
+        }
+        public override bool Contains(int Mx, int My) {
+            double length = Math.Sqrt((Mx - (X)) * (Mx - (X)) + (My - (Y)) * (My - (Y)));
+            if (length < size) return true;
+            else return false;
+        }
+        public CircleNode(int x = 0, int y = 0) : base(x, y) { }
+    }
+
+    class SquareNode : Node {
+        override public void DrawFigure(Graphics g) {
+            g.FillRectangle(new SolidBrush(Color.Green), new Rectangle(posX - size / 2, posY - size / 2, size, size));
+        }
+ 
+        
+        public override bool Contains(int Mx, int My) {
+            Rectangle n = new Rectangle(posX - size / 2, posY - size / 2, size, size);
+            if (n.Contains(Mx, My)) return true;
+            else return false;
+        }
+        public SquareNode(int x = 0, int y = 0) : base(x, y) { }
+    }
+}
